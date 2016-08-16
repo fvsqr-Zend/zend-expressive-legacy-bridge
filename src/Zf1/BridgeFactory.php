fbfb541f22b6f9cc7e504150eb317460939b8969
janatzend <jan@zend.com>
2016-08-16 14:22:57 +0200

    rename Middleware Factory

--- src/Zf1/BridgeFactory.php
+++ src/Zf1/BridgeFactory.php
@@ -3,22 +3,11 @@ namespace Zend\Expressive\LegacyBridge\Zf1;
 
 use Zend\ServiceManager\ServiceManager;
 
-class MiddlewareFactory
+class BridgeFactory
 {
     public function __invoke(ServiceManager $container)
     {
-        defined('APPLICATION_ENV')
-        || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-        
-        defined('APPLICATION_PATH')
-        || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../application'));
-        
-        set_include_path(implode(PATH_SEPARATOR, array(
-            realpath(APPLICATION_PATH . '/../src'),
-            realpath(APPLICATION_PATH . '/../vendor'),
-            zend_deployment_library_path('Zend Framework 1'),
-            get_include_path(),
-        )));
+        ($container->get('config'))['zf1-prereq']();
         
         require_once 'Zend/Application.php';
         
